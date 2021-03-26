21bd1314550d7f44d810a918ab87a49d0addf3f2
Usarneme <jamestcoop@gmail.com>
2021-03-26 11:26:49 -0700

    add a fateChooser() function to decide which language you should choose based on how terse or verbose your answers were to the questions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,36 @@
+function fateChooser(answerSet) {
+  console.log('Deciding your fate...')
+  // letter count of answers ranges from low of 58 to high of 137
+  let letterCount = 0
+  for (const ans of answerSet) {
+    letterCount += ans.length
+  }
+  if (letterCount < 60) return {"language": "J", "link": "https://code.jsoftware.com/wiki/Guides/GettingStarted"}
+  if (letterCount < 70) return {"language": "Perl", "link": "https://www.perl.org/docs.html"}
+  if (letterCount < 90) return {"language": "Go", "link": "https://golang.org/doc/"}
+  if (letterCount < 100) return {"language": "Python", "link": "https://docs.python.org/3/index.html"}
+  if (letterCount < 110) return {"language": "JavaScript", "link": "https://www.ecma-international.org/publications-and-standards/standards/ecma-262/"}
+  return {"language": "Java", "link": "https://docs.oracle.com/en/java/"}
+}
+
+// a,a,a,a,a = 59
+// a,a,a,a,b = 68
+// a,a,a,a,c = 74
+// a,a,a,b,a = 61
+// a,a,a,b,b = 70
+// a,a,a,b,c = 76
+// a,a,a,c,a = 85
+// a,a,a,c,b = 94
+// a,a,a,c,c = 100
+// a,a,b,a,a = 61
+// a,a,b,a,b = 70
+// a,a,b,a,c = 76
+// a,a,b,b,a = 63
+// a,a,b,b,b = 72
+// a,a,b,b,c = 78
+// highest is 137
+// lowest is 58
+
 $(document).ready(function() {
   // use a set so we don't get duplicate answers submitted (prevent double clicks, etc.)
   const answers = new Set()
@@ -38,7 +71,7 @@ $(document).ready(function() {
       // hide the questions
       $(".questions-container").hide()
       // ready the results for display
-      // TODO func
+      const fate = fateChooser(answers)
       // display the results
       $("results-container").show()
       // and hide the progress bar
