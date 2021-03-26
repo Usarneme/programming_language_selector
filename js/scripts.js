55fe6be5e26f7ce7ddc6d3ff449201248b067fcc
Usarneme <jamestcoop@gmail.com>
2021-03-26 09:58:43 -0700

    finishes the ui logic - all questions are now displayed one at a time and once a user clicks on one of the 3 possible multiple choice answers it hides the current and moves to the next question (if it exists). all answers are stored in a js set (to prevent double clicking duplication etc). all that remains is to handle the business logic for choosing a result and the ui logic for rendering it...

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,35 @@
 $(document).ready(function() {
+  // use a set so we don't get duplicate answers submitted (prevent double clicks, etc.)
+  const answers = new Set()
+  // get the divs containing the 5 questions
+  const questions = $("form.questions-form > div")
+  // track which question is showing using this index
+  let questionShowingIndex = 0  
+  // start by showing the first question
+  questions[questionShowingIndex].style.display = "block"
   
+  // handle clicking on any of the buttons within the form
   $("form.questions-form").submit(function(event) {
     event.preventDefault()
-    console.log(event.target)
-    // requiring the use of the form is making this way harder than it need be...
-    for (const childElement of event.target) {
-      console.log(childElement.attributes["aria-pressed"])
+    // find the button that was clicked (aka the answer that was chosen)
+    for (const buttons of event.target) {
+      // the attribute value is a string but just to be thorough check for boolean as well
+      if (buttons.attributes["aria-pressed"].value === true || buttons.attributes["aria-pressed"].value === "true") {
+        // we found a chosen answer! add it to the set of answers
+        answers.add(buttons.textContent)
+      }
+    }
+    // after updating the answers set...
+    // hide the current element
+    questions[questionShowingIndex].style.display = "none"
+    // increment which question is showing
+    questionShowingIndex += 1
+    // show the next question if it exists
+    if (questionShowingIndex < questions.length) {
+      questions[questionShowingIndex].style.display = "block"
+    } else {
+      // otherwise all questions are answered! display the results...
+      
     }
   })
-
-
-
-  // get the questions container/form
-  const questions = $("form.questions-form > div")
-  console.log(questions.length)
-  // show the first question
-  // when a question has been answered, move to the next question (while there are more questions)
-
-  // show each question one at a time until they are all answered
-  for (let i = 0; i < questions.length; i += 1) {
-    questions[i].style.display = "block"
-  }
-  // once there are no more questions
-  //  hide all questions
-  //  show the results of the survey
-
 })
